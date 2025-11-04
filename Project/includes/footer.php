ff566b76716b59d3f0441d5aaf4337c311aadcfe
jueunyang <jueunyang00@gmail.com>
2025-11-04 08:10:01 -0600

    Footer

---
+++ Project/includes/footer.php
@@ -0,0 +1,27 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manitoba Nature Archive</title>
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+<body>
+    <footer>
+        <p>&copy; <?= date("Y"); ?> Manitoba Nature Archive. All rights reserved.</p>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="index.php">Manitoba Nature Archive</a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="">Animal</a></li>
+                <li><a href="">Plant</a></li>
+                <li><a href="">Insect</a></li>
+            </ul>
+        </nav>
+</footer>
+</body>
+</html>
