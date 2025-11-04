ba269d8e5dc90559671687edfbd93df00a3ffb78
jueunyang <jueunyang00@gmail.com>
2025-11-04 08:57:47 -0600

    Main Menu Header

--- Project/includes/header.php
+++ Project/includes/header.php
@@ -8,12 +8,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manitoba Nature Archive</title>
-    <link rel="stylesheet" href="css\styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
-    <header>
-        <div class="logo">
+    <header class="main-header">
+        <div class="grid-container">
             <a href="index.php">Manitoba Nature Archive</a>
         </div>
         <nav class="main-menu">
@@ -22,8 +22,8 @@
                 <li class="main-menu-item"><a href="">Plant</a></li>
                 <li class="main-menu-item"><a href="">Insect</a></li>
                 <li class="main-menu-item">
-                    <form method="get" action="" class="sitesearch-form">
-                        <label for="sr-only" for="sitesearch">Search</label>
+                    <form method="get" action="search.php" class="sitesearch-form">
+                        <label class="sr-only" for="sitesearch">Search</label>
                         <input id="sitesearch" class="sitesearch-input" type="text" name="q" placeholder="Search">
                     </form>
                 </li>
