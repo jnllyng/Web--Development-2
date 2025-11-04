0cdfdd01f731221699a9ac03da6813fb613a93e3
jueunyang <jueunyang00@gmail.com>
2025-11-04 08:09:52 -0600

    Basic Header

---
+++ Project/includes/header.php
@@ -0,0 +1,26 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manitoba Nature Archive</title>
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="index.php">Manitoba Nature Archive</a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="">Animal</a></li>
+                <li><a href="">Plant</a></li>
+                <li><a href="">Insect</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
