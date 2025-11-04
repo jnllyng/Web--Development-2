fc8a99203a794706818f191c05dd7256bc7ba3c2
jueunyang <jueunyang00@gmail.com>
2025-11-04 08:53:50 -0600

    Main Nav Menu Search Bar

--- Project/includes/header.php
+++ Project/includes/header.php
@@ -1,26 +1,35 @@
-<?php 
+<?php
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manitoba Nature Archive</title>
     <link rel="stylesheet" href="css\styles.css">
 </head>
+
 <body>
     <header>
         <div class="logo">
-                <a href="index.php">Manitoba Nature Archive</a>
-            </div>
-        <nav class="navbar">
-            <ul class="nav-links">
-                <li><a href="">Animal</a></li>
-                <li><a href="">Plant</a></li>
-                <li><a href="">Insect</a></li>
+            <a href="index.php">Manitoba Nature Archive</a>
+        </div>
+        <nav class="main-menu">
+            <ul class="main-menu-list">
+                <li class="main-menu-item"><a href="index.php">Animal</a></li>
+                <li class="main-menu-item"><a href="">Plant</a></li>
+                <li class="main-menu-item"><a href="">Insect</a></li>
+                <li class="main-menu-item">
+                    <form method="get" action="" class="sitesearch-form">
+                        <label for="sr-only" for="sitesearch">Search</label>
+                        <input id="sitesearch" class="sitesearch-input" type="text" name="q" placeholder="Search">
+                    </form>
+                </li>
             </ul>
         </nav>
     </header>
 </body>
+
 </html>
