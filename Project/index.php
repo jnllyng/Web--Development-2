76887400ef5716ac61c12cb2404e3cb3f1f8a0d5
jueunyang <jueunyang00@gmail.com>
2025-11-04 08:17:01 -0600

    Basic Index Template

--- Project/index.php
+++ Project/index.php
@@ -1,3 +1,35 @@
-<?php 
+<?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manitoba Nature Archive</title>
+</head>
+
+<body>
+        <?php include('includes/header.php'); ?>
+
+        <main>
+            <section>
+                
+            </section>
+
+            <section>
+
+            </section>
+
+            <section>
+
+            </section>
+        </main>
+
+        <?php include('includes/footer.php'); ?>
+
+</body>
+
+</html>
